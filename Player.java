f9499a70d2a9b9c8417c4d120717b3decfa567da
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2021-03-22 13:00:56 -0700

    so rusty on java, i need a day

---
+++ Player.java
@@ -0,0 +1,12 @@
+public class Player{
+    String username;
+    int points;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+}
