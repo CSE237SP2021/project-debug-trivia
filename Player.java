bb677b0d089d03b8229beac0435aa7821d6e5656
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2021-03-22 17:13:49 -0700

    i hate java

--- Player.java
+++ Player.java
@@ -1,6 +1,13 @@
+package Player;
+
 public class Player{
     String username;
     int points;
+
+    public Player(String username, int points){
+        this.username = username;
+        this.points = points;
+    }
 
     public String getUsername(){
         return username;
