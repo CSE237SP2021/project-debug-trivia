b0fa2251efb3c56ddc1f90ef174936ee2d77e8cb
swillis1015 <sarahwillis@wustl.edu>
2021-03-26 13:04:30 -0500

    added descriptors to top of classes so we're all on the same page :P

--- Level.java
+++ Level.java
@@ -1,9 +1,7 @@
 
+// this is the level interface, each level implements it within their class
 public interface Level {
-
 	public int getCorrectAnswer();
-	public int getPlayerAnswer();
 	public int getLevelScore();
 	public String getQuestionOne();
-	public boolean isCorrect();
 }
--- Level1.java
+++ Level1.java
@@ -1,28 +1,19 @@
 
+// this is the Level1 class, it implements "level" and creates an object for Level1 which takes in
+// the questions and correct answers for the level along with the level score (increased if
+// Level1.correctAnswer == userInput in main game play.
 public class Level1 implements Level {
 	
 	int correctAnswer;
-	int playerAnswer;
 	int levelScore;
 	String questionOne;
 	
 	public Level1(String questionOne) {
 		this.questionOne = questionOne;
 	}
-
-	public boolean isCorrect() {
-		if (correctAnswer == playerAnswer) {
-			return true;
-		}
-		return false;
-	}
 	
 	public int getCorrectAnswer() {
 		return correctAnswer;
-	}
-	
-	public int getPlayerAnswer() {
-		return playerAnswer;
 	}
 	
 	public int getLevelScore() {
