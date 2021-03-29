caebae5963b75d244d908c42aa359041c62cb04c
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2021-03-28 20:03:13 -0700

    question done

--- Level1.java
+++ Level1.java
@@ -6,8 +6,8 @@ public class Level1 implements Level {
 	
 	String correctAnswer;
 	int levelScore;
-	String [] levelOneQuestions;
-	String [] levelOneAnswers;
+	String [] levelOneQuestions = new String[3];
+	String [] levelOneAnswers = new String[3];
 	
 	
 	public String getAnswer(int index) {
@@ -25,7 +25,7 @@ public class Level1 implements Level {
 	{
 		for(int i = 0; i < 3; i++) {
 			String s = Integer.toString(i);
-			this.levelOneQuestions[i] = "Please enter" + s;
+			this.levelOneQuestions[i] = "Please enter " + s;
 			this.levelOneAnswers[i] = s;
 		}
 		this.levelScore = 10;
