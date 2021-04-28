b8074cf4dde6afe3629435909c6d8a9a72b301eb
swillis1015 <sarahwillis@wustl.edu>
2021-04-27 23:03:09 -0500

    Leaderboard class confusing - not sure how to make it work. java is hard!

--- Leaderboard.java
+++ Leaderboard.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class Leaderboard {
 	
 	ArrayList<Integer> points = new ArrayList<Integer>();
+	
+	LinkedList<String> playerPoints = new LinkedList<String>();
 
-	public ArrayList<Integer> sortPlayerPoints(ArrayList<Player> players) {
+	public ArrayList<Integer> printLeaderboard(ArrayList<Player> players) {
 		for (int i=0; i < players.size(); i++) {
 			Player examinePlayer = players.get(i);
 			points.add(examinePlayer.getPoints());
 		}
 		Collections.sort(points);
 		return points;
+	}
+	
+	// really confused about this
+	public boolean printLeaderboardDemo(ArrayList<Player> players) {
+		sortPlayerPoints(PlayerStorage.getPlayerList());
+		for (int i=0; i < playerPoints.size(); i++)
+		System.out.println()
+		
 	}
 }
--- PlayerStorage.java
+++ PlayerStorage.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 
 public class PlayerStorage {
 
-	ArrayList<Player> players = new ArrayList<Player>();
+	static ArrayList<Player> players = new ArrayList<Player>();
+	
+	public ArrayList<Player> getPlayerList(){
+		return players;
+	}
 
 	public boolean addPlayer(Player newPlayer) {
 		if (players.contains(newPlayer)) {
